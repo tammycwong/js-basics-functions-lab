39993d0b5838c806ac3ec4f09f2127f676e4d407
Tammy <tammycwong@gmail.com>
2021-03-16 09:54:02 -0400

    finished

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blockNumber){
+    if (blockNumber > 42) {
+        return blockNumber - 42;
+    } else {
+        return 42 - blockNumber;
+    }
+}
+function distanceFromHqInFeet(blockNumber) {
+    return distanceFromHqInBlocks(blockNumber) * 264;
+}
+function distanceTravelledInFeet(start, destination) {
+    if (start > destination) {
+        return (start - destination) * 264;
+    }else{
+        return (destination - start) * 264;
+    }
+}
+function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start,destination);
+    if (distance <= 400) {
+        return 0;
+    }else if (distance > 400 && distance <= 2000) {
+        return (distance-400) * 0.02;
+    }else if(distance > 2000 && distance < 2500) {
+        return 25;
+    } else {
+        return "cannot travel that far";
+    }
+}
